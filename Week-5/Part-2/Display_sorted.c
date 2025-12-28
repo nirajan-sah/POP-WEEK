19ffcc197045640a6416ff385bbf4d781f7dd28a
Nirajan Sah <Nirajan2.Sah@live.uwe.ac.uk>
2025-12-28 17:38:21 +0545

    finish week-5

---
+++ Week-5/Part-2/Display_sorted.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+int Display_Sorted(int a, int b, int c) {
+    int first, second, third;
+    if (a <= b && a <= c) {
+        first = a;
+        if (b <= c) {
+            second = b;
+            third = c;
+        } else {
+            second = c;
+            third = b;
+        }
+    } else if (b <= a && b <= c) {
+        first = b;
+        if (a <= c) {
+            second = a;
+            third = c;
+        } else {
+            second = c;
+            third = a;
+        }
+    } else {
+        first = c;
+        if (a <= b) {
+            second = a;
+            third = b;
+        } else {
+            second = b;
+            third = a;
+        }
+    }
+    printf("Sorted order: %d %d %d\n", first, second, third);
+    return 0;
+}
+
+int main() {
+    int num1, num2, num3;
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    Display_Sorted(num1, num2, num3);
+    return 0;
+}
---
+++ Week-5/Part-2/password.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+void check_password_strength(char *password) {
+    int i = 0, word_count = 0, digit_count = 0, len = 0, special_count = 0;
+    for(i = 0; password[i] != '\0'; i++) 
+    {
+        if((password[i] >= 'A' && password[i] <= 'Z') ||
+           (password[i] >= 'a' && password[i] <= 'z')) {
+            word_count++;
+        } 
+        else if(password[i] >= '0' && password[i] <= '9'){
+            digit_count++;
+        }
+        else{
+            special_count++;
+        }
+        len++;   
+    }
+        if(len >= 8 && word_count > 0 && digit_count >= 2 && special_count == 0) {
+            printf("Password is valid\n");
+        } else {
+            printf("Password is invalid\n");
+        }
+    }
+int main()
+{
+    char password[100];
+    printf("Enter the password: ");
+    scanf("%s", password);
+
+    check_password_strength(password);
+    return 0;
+}
